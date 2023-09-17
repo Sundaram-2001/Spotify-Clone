ac31c7d9ed4752e83b0bdc16ad9cd9152baec5c7
sundaram <sundaram.2911@gmail.com>
2023-09-18 02:41:09 +0530

    Started working on temp page

--- src/temp.jsx
+++ src/temp.jsx
@@ -1,6 +1,12 @@
-import React from "react";
+import React from "react"
 export default function temp(){
     return(
-        <h3 style={{color:"white"}}>This is the temp-user login page</h3>
+        <div className="left-nav">
+            <div className="home-search-nav">
+                <ul>
+                    <li></li>
+                </ul>
+        </div>
+        </div>
     )
 }
