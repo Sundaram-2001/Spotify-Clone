66de9edda6ea02c89f130d3a49479af16de7091a
sundaram <sundaram.2911@gmail.com>
2023-09-19 00:00:42 +0530

    Started Working on the temp page

--- src/temp.jsx
+++ src/temp.jsx
@@ -1,12 +1,25 @@
-import React from "react"
-export default function temp(){
-    return(
-        <div className="left-nav">
-            <div className="home-search-nav">
-                <ul>
-                    <li></li>
-                </ul>
+import React from "react";
+import './temp.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouse, faSearch } from '@fortawesome/free-solid-svg-icons';
+
+export default function temp() {
+    return (
+        <div className="main-container">
+            <div className="left-nav">
+                <div className="first-section">
+                    <ul>
+                        <li className="nav-item">
+                            <FontAwesomeIcon icon={faHouse} inverse />
+                            <h3>Home</h3>
+                        </li>
+                        <li className="nav-item">
+                            <FontAwesomeIcon icon={faSearch} inverse />
+                            <h3>Search</h3>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
-        </div>
-    )
+    );
 }
