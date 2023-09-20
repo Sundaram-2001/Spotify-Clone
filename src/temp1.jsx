3da82163cea310f1456c4aef6bc71252b3a81a52
sundaram <sundaram.2911@gmail.com>
2023-09-20 08:41:26 +0530

    Worked on Navigation Bar

--- src/temp1.jsx
+++ src/temp1.jsx
@@ -1,14 +1,30 @@
 import React from "react"
+// import { useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHouse, faSearch } from '@fortawesome/free-solid-svg-icons';
-import './temp.css'
+import './temp1.css'
 export default function temp(){
+    // const [isHovered,setIsHovered]=useState(false);
+    // const handleMouseEnter=()=>{
+    //     setIsHovered(true);
+    // }
+    // const handleMouseLeave=()=>{
+    //     setIsHovered(false);
+    // }
     return(
-        <div className="main-container" style={{"color": "white"}}>
+        <div className="first-container" style={{"color": "white"}}
+            
+        >
             <ul style={{ listStyle: 'none', padding: '0'}}>
-                <li>
-                    <span><FontAwesomeIcon icon={faHouse}/></span>
-                    Home
+                    <li className="home-comp">
+                        <span><FontAwesomeIcon icon={faHouse}/></span>{' '}
+                        <span><a href="/">Home</a></span>
+                    </li>
+                
+                <br />  <br/>
+                <li className="search-comp">
+                    <span><FontAwesomeIcon icon={faSearch}/></span> {' '}
+                    Search
                 </li>
             </ul> 
         </div>
