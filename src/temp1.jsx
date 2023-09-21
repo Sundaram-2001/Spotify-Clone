23d2d81363baaca5ec5a3674d66fa67495bc22a0
sundaram <sundaram.2911@gmail.com>
2023-09-21 21:59:24 +0530

    Added some more elements in navigation bar(temp)

--- src/temp1.jsx
+++ src/temp1.jsx
@@ -1,8 +1,9 @@
 import React from "react"
 // import { useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHouse, faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faHouse, faIcons, faSearch } from '@fortawesome/free-solid-svg-icons';
 import './temp1.css'
+import { faNapster } from "@fortawesome/free-brands-svg-icons";
 export default function temp(){
     // const [isHovered,setIsHovered]=useState(false);
     // const handleMouseEnter=()=>{
@@ -12,21 +13,37 @@ export default function temp(){
     //     setIsHovered(false);
     // }
     return(
+        <>
         <div className="first-container" style={{"color": "white"}}
             
         >
             <ul style={{ listStyle: 'none', padding: '0'}}>
                     <li className="home-comp">
+                    <a href="/" style={{"textDecoration":"none","color":"white","borderRadius":"20px"}}>    
                         <span><FontAwesomeIcon icon={faHouse}/></span>{' '}
-                        <span><a href="/">Home</a></span>
+                        <span>Home</span>
+                    
+                    </a>
                     </li>
                 
                 <br />  <br/>
                 <li className="search-comp">
+                    <a href="/" style={{"textDecoration":"none","color":"white","borderRadius":"20px"}}>
                     <span><FontAwesomeIcon icon={faSearch}/></span> {' '}
-                    Search
+                    <span>Search</span>
+                    </a>
                 </li>
             </ul> 
         </div>
+        <div className="second-container">
+            <ul style={{listStyle:'none',padding:'0'}}>
+                <li className="library">
+                    <span><FontAwesomeIcon icon={faNapster}/></span>{' '}{' '}
+                    <span>Your Library</span>
+                    <span>&nbsp;&nbsp;&nbsp;&nbsp;Hello</span>
+                </li>
+            </ul>
+        </div>
+        </>
     )
 }
